e6fa35214736141c61a75e7abf4ada709f59022e
Jan Schnitzler <jan.schnitzler@gfiu.de>
2017-06-26 11:34:45 +0200

    preparing parsing functions

--- ga-to-excel.js
+++ ga-to-excel.js
@@ -1,2 +1,49 @@
-const csvParse = require('csv-parse');
+'use strict';
+
+const csvParse = require('csv-parse/lib/sync');
 const csvStringify = require('csv-stringify');
+const fs = require('fs');
+
+const gaToExcel = {};
+
+const readFile = filename => {
+  let data = csvParse(fs.readFileSync(filename), {separator: '\t', columns: true});
+
+  return data;
+};
+
+
+const replaceUsNumbers = csvLineObject => {
+  console.log(csvLineObject);
+  for (var key in csvLineObject) {
+    if (csvLineObject.hasOwnProperty(key)) {
+      csvLineObject[key] = csvLineObject[key].replace(/(\d+)\.(\d+)/, '$1,$2');
+    }
+  }
+  console.log(csvLineObject);
+  return csvLineObject;
+};
+
+
+const parseAndReplace = fileContents => {
+  let replacedData = fileContents.map(replaceUsNumbers);
+  console.log(replacedData);
+  return replacedData;
+};
+
+
+const writeOutputFile = outputData => {
+  csvStringify.stringify(outputData, {delimiter: '\t', header: true}, (err, data) => {
+    if(err) {
+      console.error(err);
+    }
+    fs.writefileSync(data, 'output/result.tsv');
+  });
+};
+
+const main = () => {
+  let data = readFile('input/test.tsv');
+
+  data = parseAndReplace(data);
+  writeOutputFile(data);
+};
